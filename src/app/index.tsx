9c01187ebff508c9f694df78fd887b70871c513a
adrianojsle <oadrianoleite@gmail.com>
2023-05-18 17:34:47 -0300

    Configuração Expo Router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,7 @@
 import { SafeAreaView } from 'react-native'
 
 export default function HomeScreen() {
-    return <SafeAreaView className="flex-1 bg-neutral-200"></SafeAreaView>
+    return <SafeAreaView className="flex-1 bg-black">
+
+    </SafeAreaView>
 }
