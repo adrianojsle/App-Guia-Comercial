208e446cfa1ea33dcbf6c69d412bddbdf1f0e0d8
adrianojsle <oadrianoleite@gmail.com>
2023-05-18 17:18:40 -0300

    Configuração do Nativewind + Home

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="nativewind/types" />
+
+
+declare global {
+  declare module '*.png' {
+    const value: import('react-native').ImageSourcePropType;
+    export default value;
+  }
+
+  declare module '*.jpg' {
+    const value: import('react-native').ImageSourcePropType;
+    export default value;
+  }
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,5 @@
+import { SafeAreaView } from 'react-native'
+
+export default function HomeScreen() {
+    return <SafeAreaView className="flex-1 bg-neutral-200"></SafeAreaView>
+}
